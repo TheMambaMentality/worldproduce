c1de192f11a707d3533d46f43fe9b2de42b687a8
EC2 Default User <ec2-user@ip-172-31-42-42.ap-northeast-1.compute.internal>
2026-01-29 16:02:58 +0000

    Update store page layout and controller

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -7,7 +7,8 @@ class StoreController < ApplicationController
         address: "大阪府堺市南区鉢ヶ峯寺2405-1",
         tel: "072-296-9911",
         image: "world_insect_reptile.jpg",
-        map_url: "https://www.google.com/maps?q=大阪府堺市南区鉢ヶ峯寺2405-1"
+        map_url: "https://www.google.com/maps?q=大阪府堺市南区鉢ヶ峯寺2405-1",
+        instagram_url: "https://www.instagram.com/konchu__hachuruikan/"
       },
       {
         name: "輝く昆虫ワールド",
@@ -15,7 +16,8 @@ class StoreController < ApplicationController
         address: "兵庫県南あわじ市八木養宜上1401",
         tel: "0799-43-2626",
         image: "shining_insect_world.jpg",
-        map_url: "https://www.google.com/maps?q=兵庫県南あわじ市八木養宜上1401"
+        map_url: "https://www.google.com/maps?q=兵庫県南あわじ市八木養宜上1401",
+        instagram_url: "https://www.instagram.com/englandkonchu/"
       },
       {
         name: "クリスタルアドベンチャー",
@@ -23,7 +25,8 @@ class StoreController < ApplicationController
         address: "兵庫県南あわじ市八木養宜上1401",
         tel: "0799-43-2626",
         image: "crystal_adventure.jpg",
-        map_url: "https://www.google.com/maps?q=兵庫県南あわじ市八木養宜上1401"
+        map_url: "https://www.google.com/maps?q=兵庫県南あわじ市八木養宜上1401",
+        instagram_url: "https://www.instagram.com/englandkonchu/"
       },
       {
         name: "昆虫爬虫類館",
@@ -31,7 +34,8 @@ class StoreController < ApplicationController
         address: "兵庫県神崎郡神河町猪篠1868",
         tel: "0790-32-2911",
         image: "insect_reptile_kamikawa.jpg",
-        map_url: "https://www.google.com/maps?q=兵庫県神崎郡神河町猪篠1868"
+        map_url: "https://www.google.com/maps?q=兵庫県神崎郡神河町猪篠1868",
+        instagram_url: "https://www.instagram.com/yodel_junglehureai/"
       },
       {
         name: "クリスタル博士のトレジャーストーン研究所",
@@ -39,7 +43,8 @@ class StoreController < ApplicationController
         address: "和歌山県和歌山市毛見1527",
         tel: "0570-064-358",
         image: "crystal_doctor_treasure.jpg",
-        map_url: "https://www.google.com/maps?q=和歌山県和歌山市毛見1527"
+        map_url: "https://www.google.com/maps?q=和歌山県和歌山市毛見1527",
+        instagram_url: "https://www.instagram.com/poruto_europe/"
       },
       {
         name: "ふれあい昆虫・爬虫類館",
@@ -47,7 +52,8 @@ class StoreController < ApplicationController
         address: "岡山県赤磐市仁堀中2006",
         tel: "086-958-2111",
         image: "fureai_insect_reptile.jpg",
-        map_url: "https://www.google.com/maps?q=岡山県赤磐市仁堀中2006"
+        map_url: "https://www.google.com/maps?q=岡山県赤磐市仁堀中2006",
+        instagram_url: "https://www.instagram.com/doitsunomori/"
       },
       {
         name: "世界の昆虫爬虫類館",
@@ -55,7 +61,8 @@ class StoreController < ApplicationController
         address: "長野県塩尻市北小野相吉5050",
         tel: "0263-88-9034",
         image: "world_insect_reptile_shinshu.jpg",
-        map_url: "https://www.google.com/maps?q=長野県塩尻市北小野相吉5050"
+        map_url: "https://www.google.com/maps?q=長野県塩尻市北小野相吉5050",
+        instagram_url: "https://www.instagram.com/shiojiri_tirol/"
       }
     ]
   end
